81910bc50e1ff9b11e7564fd33ffc78651cfb26e
CLindo01 <77864264+CLindo01@users.noreply.github.com>
2023-12-06 11:35:10 -0500

    Updated robot_car_code.ino to rpbptcar2
    This just might work, might need to reset the robot a few times.

--- robot_car_code.ino
+++ robot_car_code.ino
@@ -1,4 +1,4 @@
-#include <Servo.h> 
+#include <Servo.h>
 
 #define RightEnable 6
 #define RightForward 8
@@ -7,84 +7,106 @@
 #define LeftForward 4
 #define LeftBackward 2
 
-Servo servoLook;     //create an object to control servos
 
-const byte trig = A1;     //assigns US pins
+Servo servoLook; // create an object to control servos
+
+const byte trig = A1; // assigns US pins
 const byte echo = A0;
-const byte maxDist = 150;   //max seeing distance
-const byte stopDist = 50;   // min dustance from an object to stop in cm
-const float timeOut = 2 * (maxDist + 10) / 100 / 340 * 1000000;     //time to wait for signal
+const byte stopDist = 50; // min distance from an object to stop in cm
+const float timeOut = 2 * (stopDist + 10) / 100 / 340 * 1000000; // time to wait for signal
 
-const byte motorSpeed = 150;     //max motor speed
-const int motorOffset = 50;     // adjusts speed
-const int turnSpeed = 100;       //amount to replace when turning
+const byte motorSpeed = 125; // max motor speed
+const int turnSpeed = 50;   // amount to replace when turning
 
 void setup() {
-  pinMode(RightEnable, OUTPUT);       //set the motors to the motor speed
+  pinMode(RightEnable, OUTPUT); // set the motors to the motor speed
   pinMode(RightForward, OUTPUT);
   pinMode(RightBackward, OUTPUT);
   pinMode(LeftEnable, OUTPUT);
   pinMode(LeftForward, OUTPUT);
   pinMode(LeftBackward, OUTPUT);
-  
 
-  servoLook.attach(A2);       //assigns servo pin
+  servoLook.attach(A2); // assigns servo pin
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
 
   stopMove();
+  servoLook.write(90); // Set sensor to 90 degrees
+  delay(750); // Delay for servo to reach position
 }
 
 void loop() {
-  servoLook.write(90);        //set servo to look straight ahead
-  delay(750);
-  int distance = getDistance();   //check that there are no objects
+  unsigned long startTime = millis(); // for tracking time
+  bool obstacleDetected = false;
+  bool checkedSurroundings = false; // Flag to track if surroundings have been checked
 
-  if (distance >= stopDist) {
-    moveForward();
-  }
+  while (millis() - startTime < 4000) { // Stop if robot travels for 4 seconds without stopping
+    servoLook.write(90); // set servo to look straight ahead
+    delay(750);
+    int distance = getDistance(); // check that there are no objects
 
-  while (distance >= stopDist) {       //keep checking the object distance until it is within the minimum stopping distance
-    distance = getDistance();
-    delay(250);
-  }
+    if (distance >= stopDist && !checkedSurroundings) { // Check surroundings only if not already checked
+      moveForward();
+      unsigned long moveStartTime = millis(); // track time when robot moves
 
-  stopMove();             // Stops motors
-  int turnDir = checkDirection();              //checks left and right
-  Serial.print(turnDir);
-  switch (turnDir) {                //Turn left, turn around, or turn right
-    case 0:               //turn left
-      turnLeft(400);
-      break;
-    case 1:    //turn around
-      turnLeft(700);
-      break;
-    case 2:
-      turnRight(400);
-      break;
+      while (millis() - moveStartTime < 2000) { // continue checking surroundings for 2 seconds after moving
+        distance = getDistance();
+        if (distance < stopDist) {
+          obstacleDetected = true;
+          stopMove(); // Stop if an obstacle is detected within 2 seconds of moving
+          delay(1000); // Wait for 1 second after stopping
+          int turnDir = checkDirection(); // Check surroundings in all directions
+          Serial.print(turnDir);
+          switch (turnDir) { // Turn left, reverse, or turn right
+            case 0: // turn left
+              turnLeft(400);
+              delay(1000); // additional delay after turning left
+              break;
+            case 1: // reverse
+              moveBackward();
+              delay(2000); // reverse for 2 seconds
+              stopMove(); // stop after reversing
+              break;
+            case 2: // turn right
+              turnRight(400);
+              delay(1000); // additional delay after turning right
+              break;
+          }
+          obstacleDetected = false; // Reset obstacle detection for the next cycle
+          checkedSurroundings = true; // Set flag to indicate surroundings have been checked
+          break; // Exit the while loop after taking action
+        }
+        delay(250);
+      }
+    } else if (!obstacleDetected) {
+      stopMove(); // Stops motors if an obstacle is detected initially
+      int turnDir = checkDirection(); // checks left and right
+      Serial.print(turnDir);
+      switch (turnDir) { // Turn left, reverse, or turn right
+        case 0: // turn left
+          turnLeft(400);
+          delay(1000); // additional delay after turning left
+          break;
+        case 1: // reverse
+          moveBackward();
+          delay(2000); // reverse for 2 seconds
+          stopMove(); // stop after reversing
+          break;
+        case 2: // turn right
+          turnRight(400);
+          delay(1000); // additional delay after turning right
+          break;
+      }
+      break; // exit loop after taking one action
+    }
   }
-}
-
-void accelerate() {
-  for (int i = 0; i < motorSpeed; i++) {
-    analogWrite(RightEnable, i);
-    analogWrite(LeftEnable, i);
-    delay(10);
-  }
-}
-
-void decelerate() {
-  for (int i = motorSpeed; i != 0; i--) {
-    analogWrite(RightEnable, i);
-    analogWrite(LeftEnable, i);
-    delay(10);
-  }
+  // If the robot has traveled for 4 seconds without stopping, end program
 }
 
 void moveForward() {
   digitalWrite(RightBackward, LOW);
   digitalWrite(LeftBackward, LOW);
-  analogWrite(RightEnable, motorSpeed);
+  analogWrite(RightEnable, motorSpeed + 20);
   analogWrite(LeftEnable, motorSpeed);
   digitalWrite(RightForward, HIGH);
   digitalWrite(LeftForward, HIGH);
@@ -102,28 +124,35 @@ void stopMove() {
 void turnLeft(int duration) {
   digitalWrite(RightBackward, LOW);
   digitalWrite(LeftForward, LOW);
-  analogWrite(RightEnable, motorSpeed + turnSpeed + motorOffset);
-  analogWrite(LeftEnable, motorSpeed + turnSpeed);
+  analogWrite(RightEnable, motorSpeed);
+  analogWrite(LeftEnable, 100);
   digitalWrite(RightForward, HIGH);
   digitalWrite(LeftBackward, HIGH);
   delay(duration);
   stopMove();
 }
 
+void moveBackward() {
+  digitalWrite(RightForward, LOW);
+  digitalWrite(LeftForward, LOW);
+  analogWrite(RightEnable, motorSpeed);
+  analogWrite(LeftEnable, motorSpeed);
+  digitalWrite(RightBackward, HIGH);
+  digitalWrite(LeftBackward, HIGH);
+}
+
 void turnRight(int duration) {
   digitalWrite(RightForward, LOW);
   digitalWrite(LeftBackward, LOW);
-  analogWrite(RightEnable, motorSpeed + turnSpeed);
-  analogWrite(LeftEnable, motorSpeed + turnSpeed + motorOffset);
+  analogWrite(RightEnable, 100);
+  analogWrite(LeftEnable, motorSpeed);
   digitalWrite(RightBackward, HIGH);
   digitalWrite(LeftForward, HIGH);
   delay(duration);
   stopMove();
 }
 
-
-int getDistance()
-{
+int getDistance() {
   unsigned long pulseTime;
   int distance;
   digitalWrite(trig, HIGH);
@@ -134,26 +163,27 @@ int getDistance()
   return distance;
 }
 
-int checkDirection()
-{
-  int distances [2] = {0,0};
-  int turnDir = 1;
+int checkDirection() {
+  int distances[2] = {0, 0};
   servoLook.write(180);
   delay(500);
-  distances [0] = getDistance();
+  distances[0] = getDistance();
   servoLook.write(0);
   delay(1000);
   distances[1] = getDistance();
-  if (distances[0] >= 200 && distances [1] >= 200)
+  
+  int turnDir = 1; // Default turnDir value
+
+  if (distances[0] >= 200 && distances[1] >= 200) {
     turnDir = 0;
-  else if (distances[0] <= stopDist && distances [1] <= stopDist)
+  } else if (distances[0] <= stopDist && distances[1] <= stopDist) {
     turnDir = 1;
-  else if (distances[0] >= distances [1])
+  } else if (distances[0] >= distances[1]) {
     turnDir = 0;
-  else if (distances[0] < distances [1])
+  } else if (distances[0] < distances[1]) {
     turnDir = 2;
+  }
+  servoLook.write(90); // Return servo to 90-degree position
+  delay(500); // Delay to ensure servo reaches the desired position
   return turnDir;
 }
-
-
-
